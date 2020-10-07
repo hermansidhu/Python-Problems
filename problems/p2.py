59ee7067713ac349d42bc0da072d55494b5b3b01
Herman <44019700+sidhuherman@users.noreply.github.com>
2020-10-07 10:09:54 +0530

    Create p2.py

---
+++ problems/p2.py
@@ -0,0 +1,6 @@
+# program to calculate the bill amount to be paid by a customer based on the list of
+# gems and quantity purchased. Any purchase with a total bill amount above Rs.30000
+# is entitled for 5% discount. If any gem required by the customer is not available in the
+# store, then consider total bill amount to be -1. Assume that quantity required by the
+# customer for any gem will always be greater than 0. Perform case-sensitive
+# comparison wherever applicable.
